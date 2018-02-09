8098fcba6acbd2c1e09acd1f00e984aab8c34131
Luke Bertram <lukebertram@gmail.com>
2018-02-09 13:02:28 -0800

    Add Header to App component outside of router switch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from './Header';
 import TapList from './TapList';
 import Error404 from './Error404';
 
 function App(){
   return(
-    <Switch>
-      <Route exact path='/' component={TapList} />
-      <Route component={Error404} />
-    </Switch>
+    <div>
+      <style global jsx>{`
+        
+      `}</style>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={TapList} />
+        <Route component={Error404} />
+      </Switch>
+    </div>
   );
 }
 
