1271b5562e6802329585f74daad862fb7b68eaf9
Luke Bertram <lukebertram@gmail.com>
2018-02-09 10:45:57 -0800

    define basic app component with router switch and temp test component

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Error404 from './Error404';
+
+function App(){
+  return(
+    <Switch>
+      <Route exact path='/' component={Test} />
+    </Switch>
+  );
+}
+
+//delete the following component after testing functionality
+function Test(){
+  return(
+    <h1>Test Component, App, and Router operational</h1>
+  );
+}
+
+export default App;
