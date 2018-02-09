d7679ca26ef8a6c36b9637bdee89b5f613f6e557
Luke Bertram <lukebertram@gmail.com>
2018-02-09 12:59:53 -0800

    define prop types and structure Tap component

--- src/components/Tap.jsx
+++ src/components/Tap.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Tap(){
+function Tap(props){
   return(
     <div>
-      <h1>Tap works</h1>
+      <h1>{props.name}</h1>
+      <p>${props.price}</p>
+      <p>{props.brewery}</p>
+      <p>{props.alcoholContent}% ABV</p>
+      <p>{props.pintsRemaining}</p>
     </div>
   );
+}
+
+Tap.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  brewery: PropTypes.string,
+  alcoholContent: PropTypes.number,
+  pintsRemaining: PropTypes.number
 }
 
 export default Tap;
